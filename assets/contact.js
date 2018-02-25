34854cd3a3846902ea68b23bd4f67b8e4394d65e
Sean Andersen <smandersen13@gmail.com>
2018-02-25 10:12:23 -0500

    contact form

---
+++ assets/contact.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $("#submit").on("click", function(event){
+    alert('Under Development')
+    event.preventDefault()
+    $('#name').val("")
+    $("#email").val("")
+    $("#message").val("")
+  })
+
+})
